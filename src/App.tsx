2705a23082808de96ce9e83a01e36dc1cace8b8a
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2021-12-22 22:02:07 +0100

    Submit username to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { UserSearch } from './components/UserSearch'
 
 function App() {
+  const onUsernameSubmit = useCallback((username: string) => {
+    console.log('Submitted:', username)
+  }, [])
+
   return (
     <div className="App">
       <Header />
       <main>
-        <UserSearch />
+        <UserSearch onUsernameSubmit={onUsernameSubmit} />
       </main>
     </div>
   )
--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserSearch.css'
 import IconSearch from '../svg/IconSearch'
 
-export function UserSearch() {
+type UserSearchProps = {
+  onUsernameSubmit: (username: string) => void
+}
+
+export function UserSearch({ onUsernameSubmit }: UserSearchProps) {
+  const [username, setUsername] = useState('')
+
   return (
-    <form className="UserSearch">
+    <form
+      className="UserSearch"
+      onSubmit={(e) => {
+        e.preventDefault()
+        onUsernameSubmit(username)
+      }}
+    >
       <label htmlFor="search" className="visually-hidden">
         Search GitHub username
       </label>
@@ -14,8 +26,10 @@ export function UserSearch() {
         id="search"
         className="UserSearch-input"
         placeholder="Search GitHub username…"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <span className="UserSearch-error">No results</span>
+      {/* <span className="UserSearch-error">No results</span> */}
       <button type="submit" className="UserSearch-submit">
         Search
       </button>

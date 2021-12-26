499e2bd2ef58963b9b536e3153ed314cc6abd262
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2021-12-26 13:10:10 +0100

    Add UserDetails component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React, { useCallback } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { UserDetails } from './components/UserDetails'
 import { UserSearch } from './components/UserSearch'
+import { User } from './types'
+
+const user: User = {
+  login: 'elidrissidev',
+  avatar_url: 'https://avatars.githubusercontent.com/u/67818913?v=4',
+  name: 'Mohamed ELIDRISSI',
+  company: null,
+  blog: 'https://www.elidrissi.dev',
+  location: 'Morocco',
+  bio: 'Open-source contributor. Linux fanboy 🐧. Music is my therapy 🎵. Being different is being human.',
+  twitter_username: null,
+  public_repos: 19,
+  followers: 2,
+  following: 0,
+  created_at: '2020-07-04T06:38:42Z',
+}
 
 function App() {
   const onUsernameSubmit = useCallback((username: string) => {
@@ -13,6 +30,7 @@ function App() {
       <Header />
       <main>
         <UserSearch onUsernameSubmit={onUsernameSubmit} />
+        <UserDetails user={user} />
       </main>
     </div>
   )
---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import './UserDetails.css'
+import { User } from '../types'
+import IconLocation from '../svg/IconLocation'
+import IconWebsite from '../svg/IconWebsite'
+import IconTwitter from '../svg/IconTwitter'
+import IconCompany from '../svg/IconCompany'
+
+type UserDetailsProps = {
+  user: User
+}
+
+export function UserDetails({ user }: UserDetailsProps) {
+  const joinDate = new Date(user.created_at).toLocaleString('en-US', {
+    dateStyle: 'medium',
+  })
+
+  return (
+    <section className="UserDetails">
+      <img
+        className="UserDetails-avatar"
+        src={user.avatar_url}
+        width="70"
+        alt={user.login}
+      />
+      <div className="UserDetails-header">
+        <img
+          className="UserDetails-mobileavatar"
+          src={user.avatar_url}
+          alt={user.login}
+        />
+        <div className="UserDetails-header-details">
+          <h2 className="UserDetails-name">{user.name}</h2>
+          <span className="UserDetails-username">@{user.login}</span>
+          <time className="UserDetails-joindate" dateTime={user.created_at}>
+            Joined {joinDate}
+          </time>
+        </div>
+      </div>
+      <p className="UserDetails-bio">
+        {user.bio ?? (
+          <span style={{ opacity: 0.75 }}>This profile has no bio</span>
+        )}
+      </p>
+      <ul className="UserDetails-stats">
+        <li className="UserDetails-stat">
+          <h3 className="stat-title">Repos</h3>
+          <span className="stat-value">{user.public_repos}</span>
+        </li>
+        <li className="UserDetails-stat">
+          <h3 className="stat-title">Followers</h3>
+          <span className="stat-value">{user.followers}</span>
+        </li>
+        <li className="UserDetails-stat">
+          <h3 className="stat-title">Following</h3>
+          <span className="stat-value">{user.following}</span>
+        </li>
+      </ul>
+      <div className="UserDetails-footer">
+        <ul className="UserDetails-footer-details">
+          <li
+            className="footer-detail-item"
+            style={{ opacity: user.location ? 1 : 0.5 }}
+          >
+            <IconLocation />
+            <span>{user.location ?? 'Not available'}</span>
+          </li>
+          <li
+            className="footer-detail-item"
+            style={{ opacity: user.blog ? 1 : 0.5 }}
+          >
+            <IconWebsite />
+            <span>
+              {user.blog ? (
+                <a href={user.blog}>{user.blog}</a>
+              ) : (
+                'Not available'
+              )}
+            </span>
+          </li>
+          <li
+            className="footer-detail-item"
+            style={{ opacity: user.twitter_username ? 1 : 0.5 }}
+          >
+            <IconTwitter />
+            <span>
+              {user.twitter_username ? (
+                <a href={`https://twitter.com/${user.twitter_username}`}>
+                  {user.twitter_username}
+                </a>
+              ) : (
+                'Not available'
+              )}
+            </span>
+          </li>
+          <li
+            className="footer-detail-item"
+            style={{ opacity: user.company ? 1 : 0.5 }}
+          >
+            <IconCompany />
+            <span>{user.company ?? 'Not available'}</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
